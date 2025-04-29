ce2c60b2de33e659d8559b6d4e0b5705619e83d5
Alfo-art69 <Alfiocesarano179@gmail.com>
2025-04-29 11:17:35 +0200

    Add files via upload

---
+++ AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+[Authorize]
+public class AccountController : Controller
+{
+    private readonly BarberShop.Services.IUserService _userService;
+
+    public AccountController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet]
+    public IActionResult CompleteProfile()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> CompleteProfile(ProfileViewModel model, IFormFile diplomaFile)
+    {
+        if (ModelState.IsValid)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var result = await _userService.CompleteProfileAsync(userId, model, diplomaFile);
+
+            if (result.Success)
+            {
+                return RedirectToAction("Index", model.UserType);
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error);
+            }
+        }
+
+        return View(model);
+    }
+}
